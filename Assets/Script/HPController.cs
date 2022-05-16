600d3307f839738b2b5829922cd79476f23f2194
sakezuma2 <sakezuma@gmail.com>
2022-05-17 04:48:20 +0900

    player

---
+++ Assets/Script/HPController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HPController : MonoBehaviour
+{
+    [SerializeField, Tooltip("ñ≥ìGÉÇÅ[Éh")] bool _godMode;
+    [SerializeField, Tooltip("ç≈ëÂHP")] float _maxhp = 200;
+    float _currenthp;
+    float _damage = 10;
+
+    [SerializeField] Slider _slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.value = 1;
+        _currenthp = _maxhp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currenthp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Damege();
+    }
+    void Damege()
+    {
+        if (!_godMode)
+        {
+            var damage = _damage;
+            _currenthp = _currenthp - damage;
+            float value = _currenthp / _maxhp;
+            DOTween.To(() => _slider.value, x => _slider.value = x, value, 0.5f);
+        }
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Tooltip("Playerの動くスピード")] float _speed = 7f;
-    float _h,_v;
+    [SerializeField, Tooltip("Playerの移動スピード")] float _speed = 7f;
+    [SerializeField, Tooltip("Playerのステップ移動のスピード")] float _stepSpeed = 10f;
+    [SerializeField, Tooltip("Playerの移動制限する")] float _moveCooltime = 0.3f;
+    [SerializeField] float _stepMoveCooltime = 2f;
+    bool _isMoving = true;
+    bool _isStepMoving = false;
+    float _h, _v;
+    Vector2 _dir;
     Rigidbody2D _rb;
 
     // Start is called before the first frame update
@@ -17,8 +24,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       _h = Input.GetAxisRaw("Horizontal");
-       _v = Input.GetAxisRaw("Vertical");
-        _rb.velocity = new Vector2(_speed * _h, _speed * _v);
+        _h = Input.GetAxisRaw("Horizontal");
+        _v = Input.GetAxisRaw("Vertical");
+        _dir = new Vector2(_h, _v);
+
+        if (Input.GetButtonDown("Jump") && _isMoving && !_isStepMoving)
+        {
+            _isStepMoving = true;
+            _isMoving = false;
+            _rb.velocity = _dir == Vector2.zero ? new Vector2(_stepSpeed, _rb.velocity.y) :
+                _rb.velocity = new Vector2(_stepSpeed * _h, _stepSpeed * _v);
+            StartCoroutine(DelayMethod(_stepMoveCooltime, () => _isStepMoving = false));
+            StartCoroutine(DelayMethod(_moveCooltime, () => _isMoving = true));
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (_isMoving)
+        {
+            float speed = _dir == Vector2.zero ? 0 : _speed;
+            _rb.velocity = new Vector2(speed * _h, speed * _v);
+        }
+    }
+    private IEnumerator DelayMethod(float seconds, Action action)
+    {
+        yield return new WaitForSeconds(seconds);
+        action?.Invoke();
     }
 }
